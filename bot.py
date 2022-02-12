abb8a6822e2c8e946c5bf9b51f35571afed75647
Rael Max <contato@raelmax.com>
2022-02-12 17:20:03 -0300

    yep, i did it

---
+++ bot.py
@@ -0,0 +1,57 @@
+import re
+import os
+import random
+import logging
+
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+token = os.environ.get("RAELAOVIVOBOT_TELEGRAM_TOKEN")
+assert token is not None, "RAELAOVIVOBOT_TELEGRAM_TOKEN envvar required, idiot."
+
+
+def _get_random_audio():
+    random_number = random.randint(1, 16)
+    return open(f"./audios/{random_number}.ogg", "rb")
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    audio = _get_random_audio()
+    update.message.reply_audio(audio)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(
+        MessageHandler(Filters.regex(re.compile(r"rael", re.IGNORECASE)), echo)
+    )
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
